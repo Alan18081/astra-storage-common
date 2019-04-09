e3a2b9541dd8f13a37a13c6c8f28346c5d318baf
t.kandibolotskaya <t.kandibolotskaya@bet-makers.com>
2019-04-09 17:07:00 +0300

    fixes

--- helpers/create-client-options.js
+++ helpers/create-client-options.js
@@ -1,13 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const microservices_1 = require("@nestjs/microservices");
-const config_1 = require("../config");
-exports.createClientOptions = (queue) => {
+exports.createClientOptions = (queue, url) => {
     return {
         transport: microservices_1.Transport.RMQ,
         options: {
             queue,
-            urls: [config_1.RABBIT_MQ_URL],
+            urls: [url],
         },
     };
 };
